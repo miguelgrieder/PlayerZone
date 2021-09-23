93e55df817ff74321c4b7a3133c5b2ac0ccc585b
Migue Duarte Grieder <81339737+migueldgrieder@users.noreply.github.com>
2021-09-23 11:40:16 -0300

    ajustes botao login

--- html/js/entrar.js
+++ html/js/entrar.js
@@ -1,20 +1,5 @@
-import { selectAllUsers, selectUser, insertUser, updateUser } from '../../db/UserController'
+import { selectAllUsers, selectUser, insertUser, updateUser } from '../../db/UserController';
 
-var x = document.getElementById("login");
-var y = document.getElementById("cadastrar");
-var z = document.getElementById("btn");
-
-function cadastrar(){
-    x.style.left = "-400px";
-    y.style.left = "50px";
-    z.style.left = "110px";
-}
-
-function login(){
-    x.style.left = "50px";
-    y.style.left = "450px";
-    z.style.left = "0px";
-}
 
 function LoginDB(){
     var Usuario = selectUser(Codenome)
