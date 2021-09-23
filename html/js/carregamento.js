7d61c2af5821091b0600b8edf72827ccff208bdd
Sirgubler <matheus.gubler@gmail.com>
2021-09-23 07:16:58 -0300

    Teste API

--- html/js/carregamento.js
+++ html/js/carregamento.js
@@ -1,3 +1,17 @@
+const insereUsuario = async (user) => {
+    let resposta = await fetch("/users", 
+    {
+       method: "POST",
+       body: JSON.stringify(user)
+    });
+}
+
 function carregamento() {
+    let user = {
+        Nick: 'Matheus',
+        Email: 'xya@',
+        Password: '1234'
+    };
+    insereUsuario(user);
     alert("A PÁGINA FOI CARREGADA COM SUCESSO!\nCLIQUE NA LOGO PARA ADENTRAR NO HTML APÓS FECHAR ESSA MENSAGEM!\nALGUNS EVENTOS INCLUSOS NESTE HTML:\nONLOAD, ONMOUSEOVER, ONMOUSEOUT E ONCLICK. ")
 }
